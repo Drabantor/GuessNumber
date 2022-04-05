cea169ae551cc6ec93ebffc015307d40a8393bdf
Joakim <cloromakasol@hotmail.com>
2022-04-05 22:12:22 +0200

    Fixed some issues.

--- GuessNuber/Program.cs
+++ GuessNuber/Program.cs
@@ -13,66 +13,83 @@ namespace GuessNumber
                 Console.WriteLine("\n\tGISSA NUMRET");
                 Console.Write("\n\t[1]SPELA");
                 Console.WriteLine("\n\t[2]AVSLUTA");
-
-                int menyVal = Convert.ToInt32(Console.ReadLine());
-
+               
                 try
                 {
-                    switch (menyVal)
+                    int menyVal = Convert.ToInt32(Console.ReadLine());
+
+                    if (menyVal != 1)
                     {
-                        case 1:
-                            Random rd = new Random();
-                            int rand_num = rd.Next(1, 101);
-                            int guess;
-                            int noOfGuesses = 5;
-                            bool gameover = false;
-
-                            Console.WriteLine("\n\nGissa på ett nummer mellan 1 och 100. Du har fem försök att gissa rätt.");
-                            noOfGuesses--;
-
-                            while (gameover == false)
-                            {
-                                guess = Convert.ToInt32(Console.ReadLine());
-
-                                if (guess < 1 || guess > 100)
-                                {
-                                    Console.WriteLine("\nBara tal mellan 1 och 100! Försök igen!");
-                                    Console.ReadLine();
-                                }
-
-                                if (guess != rand_num && noOfGuesses == 0)
-                                {
-                                    Console.WriteLine("\nSpelet är slut och du kan inte gissa fler gånger. Talet jag tänkte på var: " + rand_num.ToString());
-                                    gameover = true;
-                                }
-                                else if (guess == rand_num)
-                                {
-                                    Console.WriteLine("\nDet är rätt gissat. DU VINNER!");
-                                    gameover = true;
-                                }
-                                else if (guess > rand_num)
-                                {
-                                    noOfGuesses--;
-                                    Console.WriteLine("\nTalet är för högt. Försök igen!");
-                                }
-                                else if (guess < rand_num)
-                                {
-                                    noOfGuesses--;
-                                    Console.WriteLine("\nTalet är för lågt. Försök igen!");
-                                }
-                            }
-                            Console.ReadLine();
-                            break;
-
-                        case 2:
-                            isRunning = false;
-                            break;
+                        Console.WriteLine("\nFel sifferval! Du kan bara välja siffrorna 1 eller 2.");
+                    
                     }
 
+                    {
+                        switch (menyVal)
+                        {
+                            case 1:
+                                Random rd = new Random();
+                                int rand_num = rd.Next(1, 101);
+                                int guess;
+
+                                int noOfGuesses = 5;
+                                bool gameover = false;
+
+                                Console.WriteLine("\n\nGissa på ett nummer mellan 1 och 100. Du har fem försök att gissa rätt.");
+                                noOfGuesses--;
+
+                                while (gameover == false)
+                                {
+                                    try
+                                    {
+                                        guess = Convert.ToInt32(Console.ReadLine());
+
+                                        if (guess < 1 || guess > 100)
+                                        {
+                                            Console.WriteLine("\nBara tal mellan 1 och 100! Försök igen!");
+                                            Console.ReadLine();
+                                        }
+
+                                        if (guess != rand_num && noOfGuesses == 0)
+                                        {
+                                            Console.WriteLine("\nSpelet är slut och du kan inte gissa fler gånger. Talet jag tänkte på var: " + rand_num.ToString() + " \nTryck ENTER för att forsätta.");
+                                            gameover = true;
+                                        }
+                                        else if (guess == rand_num)
+                                        {
+                                            Console.WriteLine("\nDet är rätt gissat. DU VINNER! Tryck ENTER för att forsätta.");
+                                            gameover = true;
+                                        }
+                                        else if (guess > rand_num)
+                                        {
+                                            noOfGuesses--;
+                                            Console.WriteLine("\nTalet är för högt. Försök igen!");
+                                        }
+                                        else if (guess < rand_num)
+                                        {
+                                            noOfGuesses--;
+                                            Console.WriteLine("\nTalet är för lågt. Försök igen!");
+                                        }
+                                    }
+                                    catch (Exception)
+                                    {
+                                        Console.WriteLine("\nFel format! Du kan bara ange heltal. \nTyck ENTER för att återgå till menyn.");
+                                        break;
+                                    }
+
+                                }
+                                Console.ReadLine();
+                                break;
+
+                            case 2:
+                                isRunning = false;
+                                break;
+                        }
+                    }
                 }
                 catch (Exception)
                 {
-                    Console.WriteLine("\n\tFel format! Du kan bara ange heltal. Åter till menyn.");
+                    Console.WriteLine("\nFel format! Du kan bara ange heltalen 1 eller 2.");
                     continue;
                 }
             }
